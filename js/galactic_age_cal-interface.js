5a8a3a1e30ec09f7e1ed1b06b6a271edc7612b3c
Javier Jimenez <javierrcc522@gmail.com>
2017-10-13 09:52:32 -0700

    add constructor to the backend

---
+++ js/galactic_age_cal-interface.js
@@ -0,0 +1,5 @@
+import {Character, Level} from './../js/rpg.js';
+
+$(document).ready(function() {
+
+});
---
+++ js/galactic_age_cal.js
@@ -0,0 +1,15 @@
+export class Age{
+  constructor(age){
+    this.age = age;
+    this.year = 365;
+    this.hour = 24;
+    this.minute = 60;
+    this.second = 60;
+
+  }
+
+  checkAge(age){
+
+
+  }
+}
